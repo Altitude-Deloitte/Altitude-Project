4de21d2a545dbd82b8d1aba4b5a8ed2c173db501
Dhingra <acdhingra@deloitte.com>
2025-07-01 11:51:31 +0530

    meme component added

---
+++ altitude-app/src/app/components/meme-creation/meme-creation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meme-creation',
+  imports: [],
+  templateUrl: './meme-creation.component.html',
+  styleUrl: './meme-creation.component.css'
+})
+export class MemeCreationComponent {
+
+}
---
+++ altitude-app/src/app/components/meme-review/meme-review.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meme-review',
+  imports: [],
+  templateUrl: './meme-review.component.html',
+  styleUrl: './meme-review.component.css'
+})
+export class MemeReviewComponent {
+
+}
